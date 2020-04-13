73ef8115035b20874bba4fdf94292cd9887b80f1
yotaroy <yotaro1996@gmail.com>
2020-04-13 14:14:16 +0900

    Fix convert_pbn

--- dataset/convert_pbn.py
+++ dataset/convert_pbn.py
@@ -2,7 +2,7 @@ import json
 from tqdm import tqdm
 
 dataset_path = 'eval/deal_5000k.json'
-pbn_file_path = 'eval/deal_5000k_1k.pbn'
+pbn_file_path = 'eval/deal_5000k.pbn'
 
 
 with open(dataset_path, 'r') as fr:
