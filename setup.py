8eb940b40e6c8c0cf4e3292307795df55028268a
Yotaro Yamaguchi <yyamaguchi643@gmail.com>
2020-10-26 17:46:18 +0900

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bridge_env',
+    version='0.1.0',
+    description='Contract bridge environment.',
+    author='yotaroy',
+    url='https://github.com/yotaroy/bridge_env',
+    author_email='yyamaguchi643@gmail.com',
+    license='MIT',
+    install_requires=['numpy'],
+    packages=['bridge_env']
+)
