91b336c543b8a1c3cf7c6bd56223c88b54d3914c
Yotaro Yamaguchi <yyamaguchi643@gmail.com>
2020-11-06 18:26:07 +0900

    Add tuple binary vector in Hands

--- tests/test_hands.py
+++ tests/test_hands.py
@@ -9,50 +9,53 @@ from tests.data_handler import HANDS1, HANDS2, HANDS3, PBN_HANDS1, PBN_HANDS2, \
 
 class TestHands:
     BINARY_HANDS1 = {
-        Player.N: np.array((0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1,
-                            1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-                            1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0,
-                            0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)),
-        Player.E: np.array((1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-                            0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0,
-                            0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0)),
-        Player.S: np.array((0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0,
-                            0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                            1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1)),
-        Player.W: np.array((0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0,
-                            0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0,
-                            0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-                            0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0))}
+        Player.N: (0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+                   1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+                   1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0,
+                   0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        Player.E: (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+                   0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0,
+                   0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0),
+        Player.S: (0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+                   0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+                   1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1),
+        Player.W: (0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0,
+                   0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0,
+                   0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+                   0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0)}
     BINARY_HANDS2 = {
-        Player.N: np.array((1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1,
-                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-                            0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0,
-                            0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1)),
-        Player.E: np.array((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                            1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
-                            0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-                            1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0)),
-        Player.S: np.array((0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0,
-                            0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0,
-                            0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-                            0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0)),
-        Player.W: np.array((0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0,
-                            0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
-                            1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0,
-                            0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0))}
+        Player.N: (1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+                   0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+                   0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1),
+        Player.E: (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+                   0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+                   1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0),
+        Player.S: (0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0,
+                   0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0,
+                   0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
+                   0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+        Player.W: (0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0,
+                   0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+                   1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0,
+                   0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0)}
     BINARY_HANDS3 = {
-        Player.N: np.zeros(52),
-        Player.E: np.array((0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-                            0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0,
-                            0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1)),
-        Player.S: np.zeros(52),
-        Player.W: np.array((0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-                            1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0,
-                            0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
-                            1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0))}
+        Player.N: tuple([0] * 52),
+        Player.E: (0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+                   0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+                   0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1),
+        Player.S: tuple([0] * 52),
+        Player.W: (0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+                   1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+                   1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0)}
+    NP_BINARY_HANDS1 = {p: np.array(b) for p, b in BINARY_HANDS1.items()}
+    NP_BINARY_HANDS2 = {p: np.array(b) for p, b in BINARY_HANDS2.items()}
+    NP_BINARY_HANDS3 = {p: np.array(b) for p, b in BINARY_HANDS3.items()}
 
     @pytest.mark.parametrize(('hands', 'dealer', 'expected'),
                              [(HANDS1, Player.N, PBN_HANDS1),
@@ -70,6 +73,15 @@ class TestHands:
         for p in Player:
             np.testing.assert_array_equal(actual[p], expected[p])
 
+    @pytest.mark.parametrize(('hands', 'expected'),
+                             [(HANDS1, NP_BINARY_HANDS1),
+                              (HANDS2, NP_BINARY_HANDS2),
+                              (HANDS3, NP_BINARY_HANDS3)])
+    def test_to_np_binary(self, hands, expected):
+        actual = hands.to_np_binary()
+        for p in Player:
+            np.testing.assert_array_equal(actual[p], expected[p])
+
     @pytest.mark.parametrize(('pbn_hands', 'expected'),
                              [(PBN_HANDS1, HANDS1),
                               (PBN_HANDS2, HANDS2),
@@ -83,3 +95,10 @@ class TestHands:
                               (BINARY_HANDS3, HANDS3)])
     def test_convert_binary(self, binary_hands, expected):
         assert Hands.convert_binary(binary_hands) == expected
+
+    @pytest.mark.parametrize(('np_binary_hands', 'expected'),
+                             [(NP_BINARY_HANDS1, HANDS1),
+                              (NP_BINARY_HANDS2, HANDS2),
+                              (NP_BINARY_HANDS3, HANDS3)])
+    def test_convert_np_binary(self, np_binary_hands, expected):
+        assert Hands.convert_np_binary(np_binary_hands) == expected
